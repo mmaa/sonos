26e5a4234a0b75a876eefdff0e01743397f7a369
Marcos Acosta <m@mmaa.co>
2020-09-08 10:17:04 -0500

    Add command for setting preferences

---
+++ lib/sonos/command.ex
@@ -0,0 +1,26 @@
+defmodule Sonos.Command do
+  alias Sonos.Client
+
+  def set_preferences(user) do
+    %{households: households} = Client.command_get(user, "/households")
+
+    for household <- households do
+      %{players: players} =
+        Client.command_get(user, "/households/#{household.id}/groups")
+
+      for player <- Enum.filter(players, &(&1.name == "Living Room")) do
+        Client.command_post(
+          user,
+          "/players/#{player.id}/homeTheater/options",
+          %{nightMode: user.night_mode, enhanceDialog: user.enhance_dialog}
+        )
+
+        Client.command_post(
+          user,
+          "/players/#{player.id}/playerVolume",
+          %{volume: 10, muted: false}
+        )
+      end
+    end
+  end
+end
